2b978165287198384f554579116b316679b2875a
dudueasy <dudueasy@126.com>
2017-12-04 23:43:57 +0800

    解决了一个bug

--- 模块3/正则处理四则运算.py
+++ 模块3/正则处理四则运算.py
@@ -3,17 +3,18 @@
 import re
 import sys
 
-# source = '1*22+(5+2)*3'
-source = input('请输入计算式')
+source = '1-3*(3+19)*(1-17+20/0)*3'
+# source = '-65.0*-48.0'
+# source = input('请输入计算式')
 
 # 检查输入是否合法
 def check(s):
     flag = True
-    if re.findall(r'[^0-9\+\-\*\/\s\(\)]',s):
+    if re.findall(r'[^0-9\+\-\.\*\/\s\(\)]',s):
         print('包含非法字符')
         flag = False
 
-    if s.count('(') != s.count(')'):
+    if s.count('(') + s.count(')')>0 and s.count('(') != s.count(')'):
         print('括号未闭合')
         flag = False
 
@@ -30,11 +31,11 @@ def formatter(s):
 
 # 计算
 def calculate(s):
-    #计算乘除
     if re.search('^\(.*\)$',s):
         s = s.lstrip('(').rstrip(')')
 
-    result = re.search('\d+\.?\d*[*/]\d+\.?\d*',s)
+    #计算乘除
+    result = re.search(r'\d+\.?\d*[*/](-)?\d+\.?\d*',s)
     while result:
         expression = result.group()
         num_1,num_2 = re.split('[*/]',expression)
@@ -43,12 +44,18 @@ def calculate(s):
         if '*' in expression:
             res =  str(num_1* num_2)
         if '/' in expression:
-            res =  str(num_1/num_2)
+            try:
+                res =  str(num_1/num_2)
+
+            except ZeroDivisionError:
+                print('0不能作为除数')
+                exit()
         s = s.replace(expression, res)
-        result = re.search('\d+\.?\d*[*/]\d+\.?\d*', s)
+        result = re.search(r'\d+\.?\d*[*/](-)?\d+\.?\d*',s)
 
     else:
         # 计算加减法
+        s = formatter(s)
         result = re.search('\d+\.?\d*[+-]\d+\.?\d*', s)
         while result:
             expression = result.group()
@@ -65,7 +72,6 @@ def calculate(s):
     # 返回计算结果
     return s
 
-
 if check(source):
     source = formatter(source)
     while re.search('\(',source):
@@ -73,5 +79,4 @@ if check(source):
         source = source.replace(num_string,calculate(num_string))
     else:
         source = calculate(source)
-
-print(source)
+    print(source)
